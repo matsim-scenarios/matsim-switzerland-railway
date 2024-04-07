0d2302633a07e1c2db7139e25a775a30681901a6
ikaddoura <ihab.kaddoura@sbb.ch>
2024-04-07 02:08:24 +0200

    remove unused imports

--- src/main/java/ch/sbb/run/RunSwitzerlandRailway.java
+++ src/main/java/ch/sbb/run/RunSwitzerlandRailway.java
@@ -23,8 +23,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.NetworkWriter;
-import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
