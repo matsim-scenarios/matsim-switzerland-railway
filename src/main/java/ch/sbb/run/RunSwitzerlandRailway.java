e29a265511d524f6377bfe4d51a9b3d07eeaf018
ikaddoura <ihab.kaddoura@sbb.ch>
2024-04-07 14:14:36 +0200

    adjust run script, nicer vehicle names and so on

--- src/main/java/ch/sbb/run/RunSwitzerlandRailway.java
+++ src/main/java/ch/sbb/run/RunSwitzerlandRailway.java
@@ -21,6 +21,7 @@ package ch.sbb.run;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.config.Config;
@@ -28,6 +29,11 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.transitSchedule.api.Departure;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
 
 import ch.sbb.matsim.contrib.railsim.RailsimModule;
 import ch.sbb.matsim.contrib.railsim.RailsimUtils;
@@ -50,11 +56,22 @@ public final class RunSwitzerlandRailway {
 		
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		
-		// set some train attributes which are used by the railsim engine
-		scenario.getTransitVehicles().getVehicleTypes().values().forEach(vehicle -> {
-			vehicle.setMaximumVelocity(27.778).setLength(500.);
-		});
+		adjustVehicles(scenario);
+		adjustNetwork(scenario);
+			
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingModule(new RailsimModule());
+
+		// if you have other extensions that provide QSim components, call their configure-method here
+		controler.configureQSimComponents(components -> new RailsimQSimModule().configure(components));
+
+		controler.run();
 		
+		log.info("Done.");
+	}
+
+	private static void adjustNetwork(Scenario scenario) {
 		// set some network attributes which are used by the railsim engine
 		int resourceCnt = 0;
 		for (Link link : scenario.getNetwork().getLinks().values()) {
@@ -71,17 +88,84 @@ public final class RunSwitzerlandRailway {
 			}
 			resourceCnt++;
 		}
-			
-		Controler controler = new Controler(scenario);
+	}
 
-		controler.addOverridingModule(new RailsimModule());
-
-		// if you have other extensions that provide QSim components, call their configure-method here
-		controler.configureQSimComponents(components -> new RailsimQSimModule().configure(components));
-
-		controler.run();
+	private static void adjustVehicles(Scenario scenario) {
+		// nicer vehicle names
+		scenario.getTransitVehicles().getVehicles().values().forEach(vehicle -> {
+			scenario.getTransitVehicles().removeVehicle(vehicle.getId());
+		});
+		scenario.getTransitVehicles().getVehicleTypes().values().forEach(vehicleType -> {
+			scenario.getTransitVehicles().removeVehicleType(vehicleType.getId());
+		});
 		
-		log.info("Done.");
+		VehicleType vehTypeIC = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("IC", VehicleType.class));
+		vehTypeIC.getCapacity().setStandingRoom(1000);
+		vehTypeIC.setMaximumVelocity(100.);
+		vehTypeIC.setLength(1000.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeIC);
+		
+		VehicleType vehTypeEC = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("EC", VehicleType.class));
+		vehTypeEC.getCapacity().setStandingRoom(1000);
+		vehTypeEC.setMaximumVelocity(100.);
+		vehTypeIC.setLength(1000.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeEC);
+		
+		VehicleType vehTypeIR = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("IR", VehicleType.class));
+		vehTypeIR.getCapacity().setStandingRoom(1000);
+		vehTypeIR.setMaximumVelocity(100.);
+		vehTypeIC.setLength(1000.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeIR);
+		
+		VehicleType vehTypeRE = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("RE", VehicleType.class));
+		vehTypeRE.getCapacity().setStandingRoom(1000);
+		vehTypeRE.setMaximumVelocity(27.777);
+		vehTypeRE.setLength(300.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeRE);
+		
+		VehicleType vehTypeS = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("S", VehicleType.class));
+		vehTypeS.getCapacity().setStandingRoom(1000);
+		vehTypeS.setMaximumVelocity(27.777);
+		vehTypeS.setLength(300.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeS);
+		
+		VehicleType vehTypeOther = scenario.getTransitVehicles().getFactory().createVehicleType(Id.create("other", VehicleType.class));
+		vehTypeOther.getCapacity().setStandingRoom(1000);
+		vehTypeOther.setMaximumVelocity(27.777);
+		vehTypeOther.setLength(300.);
+		scenario.getTransitVehicles().addVehicleType(vehTypeOther);
+		
+		for (TransitLine line : scenario.getTransitSchedule().getTransitLines().values()) {
+			for (TransitRoute route : line.getRoutes().values()) {
+				for (Departure dep : route.getDepartures().values()) {
+					
+					VehicleType newVehicleType = null;
+					if (line.getName().startsWith("IC")) {
+						newVehicleType = vehTypeIC;
+
+					} else if (line.getName().startsWith("EC")) {
+						newVehicleType = vehTypeEC;
+
+					} else if (line.getName().startsWith("IR")) {
+						newVehicleType = vehTypeIR;
+
+					} else if (line.getName().startsWith("RE")) {
+						newVehicleType = vehTypeRE;
+
+					} else if (line.getName().startsWith("S")) {
+						newVehicleType = vehTypeS;
+
+					} else {
+						newVehicleType = vehTypeOther;
+					}
+					
+					Id<Vehicle> newVehId = Id.createVehicleId("line_" + line.getName() + "_" + dep.getId());
+					dep.setVehicleId(newVehId);
+					Vehicle newVehicle = scenario.getTransitVehicles().getFactory().createVehicle(newVehId, newVehicleType);
+					scenario.getTransitVehicles().addVehicle(newVehicle);	
+				}
+			}
+		}
 	}
 
 }
